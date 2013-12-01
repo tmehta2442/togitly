0f46301c5a25586427a20d68527054a1e0f77915
TMehta2442 <tmehta@post.harvard.edu>
2013-12-01 14:47:39 -0800

    trying to send to database

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -1,6 +1,20 @@
 class UrlsController < ApplicationController
 
 	def index
+		@url = Url.new
+	end
+
+	# def new
+	# 	@url = Url.new
+	# end
+
+	def create
+		@url = Url.create(params[:website])
+		redirect_to @url
+	end
+
+	def show
+		@urls = Url.find(params[:id])
 	end
 
 	
---
+++ app/models/url.rb
@@ -0,0 +1,3 @@
+class Url < ActiveRecord::Base
+  attr_accessible :website, :hash_code
+end
