3f3e02b7d18521ed82df14c91e8f2909d071d17d
TMehta2442 <tmehta@post.harvard.edu>
2013-12-01 19:59:50 -0800

    url address saving

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -5,34 +5,16 @@ class UrlsController < ApplicationController
 
 	end
 
-	# def new
-	# 	@url = Url.new
-	# end
-
-	# def update
-	# 	hash_code = rand(10000)
-	# 	@url = Url.new(params[:website], hash_code: hash_code)
-	# 	redirect_to @url
-	# end
-
 	def create
 	 	hash_code = rand(10000)
-	 	@url = Url.new(params[:website], hash_code: hash_code)
-		# url = Url.new(website: params[:website], hash_code: hash_code)
-		# url = Url.new(website: :website, hash_code: hash_code)
-		# url = Url.new(website: params[:url], hash_code: hash_code)
-
-		# url = Url.create(website: params[:url], hash_code: hash_code)
-		# @url = Url.new(website: params[:website])
-		# @url = Url.create
-		# @url = Url.new
-		# @url.save
-		redirect_to urls_path
+	 	@url = Url.new(website: params[:url][:website], hash_code: hash_code)
+		@url.save
+		redirect_to @url
 	end
 
-	# def show
-	# 	@urls = Url.find(params[:id])
-	# end
+	def show
+		@urls = Url.all
+	end
 
 	
 end
