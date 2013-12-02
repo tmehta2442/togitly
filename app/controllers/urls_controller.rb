df6cb8eeaaea9b7390563c3b43bae14fd31e80c4
TMehta2442 <tmehta@post.harvard.edu>
2013-12-01 16:52:51 -0800

    still not there

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -2,21 +2,37 @@ class UrlsController < ApplicationController
 
 	def index
 		@url = Url.new
+
 	end
 
 	# def new
 	# 	@url = Url.new
 	# end
 
+	# def update
+	# 	hash_code = rand(10000)
+	# 	@url = Url.new(params[:website], hash_code: hash_code)
+	# 	redirect_to @url
+	# end
+
 	def create
-		hash_code = rand(10000)
-		@urls = Url.create(website: params[:id], hash_code: hash_code)
-		redirect_to @urls
+	 	hash_code = rand(10000)
+	 	@url = Url.new(params[:website], hash_code: hash_code)
+		# url = Url.new(website: params[:website], hash_code: hash_code)
+		# url = Url.new(website: :website, hash_code: hash_code)
+		# url = Url.new(website: params[:url], hash_code: hash_code)
+
+		# url = Url.create(website: params[:url], hash_code: hash_code)
+		# @url = Url.new(website: params[:website])
+		# @url = Url.create
+		# @url = Url.new
+		# @url.save
+		redirect_to urls_path
 	end
 
-	def show
-		@urls = Url.find(params[:id])
-	end
+	# def show
+	# 	@urls = Url.find(params[:id])
+	# end
 
 	
 end
